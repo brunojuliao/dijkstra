93f7f018d9151644b7c669a59441ca20f7a4262a
Bruno Julião <brunojuliao.net@gmail.com>
2020-02-12 12:58:18 -0300

    Removing the code for min heap test

--- src/graph.py
+++ src/graph.py
@@ -21,16 +21,3 @@ g = Graph({
 })
 
 #g.start("A", "E")
-
-h = MinHeap()
-h.push(Node("A", 20))
-h.push(Node("B", 10))
-h.push(Node("C", 50))
-h.push(Node("D", 40))
-h.push(Node("E", 45))
-h.push(Node("F", 60))
-h.push(Node("G", 5))
-h.print()
-temp = h.pop()
-print(temp)
-h.print()
